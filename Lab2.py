4103f547ae5ee9b35332f510056db98190ce573e
Protik <protik10.25@ichat.sp.edu.sg>
2026-05-01 14:29:13 +0800

    Added BMI calculation and classification logic

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,15 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight / (height * height)
+    print(f'Your BMI is: {bmi:.2f}')
+    
+    # Conditional logic for BMI classification
+    if bmi < 18.5:
+        print("Classification: Under Weight")
+    elif bmi >= 18.5 and bmi <= 25.0:
+        print("Classification: Normal Weight")
+    else:
+        print("Classification: Over Weight")
+
+calculate_bmi(weight=57.0, height=1.73)
